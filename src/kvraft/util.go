1464d9d547dd5f49059e1ef7407e2482acd91fb1
laptop-wsl <1251887477@qq.com>
2023-02-06 17:08:29 +0800

    add log

--- src/kvraft/util.go
+++ src/kvraft/util.go
@@ -35,23 +35,9 @@ func getVerbosity() int {
 type logTopic string
 
 const (
-	dClient  logTopic = "CLNT"
-	dKVraft  logTopic = "KVRA"
-	dCommit  logTopic = "CMIT"
-	dDrop    logTopic = "DROP"
-	dError   logTopic = "ERRO"
-	dInfo    logTopic = "INFO"
-	dLeader  logTopic = "LEAD"
-	dLog     logTopic = "LOG1"
-	dLog2    logTopic = "LOG2"
-	dPersist logTopic = "PERS"
-	dSnap    logTopic = "SNAP"
-	dTerm    logTopic = "TERM"
-	dTest    logTopic = "TEST"
-	dTimer   logTopic = "TIMR"
-	dTrace   logTopic = "TRCE"
-	dVote    logTopic = "VOTE"
-	dWarn    logTopic = "WARN"
+	dClient logTopic = "CLNT"
+	dKVraft logTopic = "KVRA"
+	dInfo   logTopic = "INFO"
 )
 
 var debugStart time.Time
