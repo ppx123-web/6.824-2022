c73888142ca05f0abc35365aeb2da7b4b274055d
zcy <1251887477@qq.com>
2023-02-15 10:56:44 +0000

    start 4B

--- src/shardkv/client.go
+++ src/shardkv/client.go
@@ -8,17 +8,19 @@ package shardkv
 // talks to the group that holds the key's shard.
 //
 
-import "6.824/labrpc"
-import "crypto/rand"
-import "math/big"
-import "6.824/shardctrler"
-import "time"
+import (
+	"crypto/rand"
+	"math/big"
+	"sync"
+	"time"
 
-//
+	"6.824/labrpc"
+	"6.824/shardctrler"
+)
+
 // which shard is a key in?
 // please use this function,
 // and please do not change it.
-//
 func key2shard(key string) int {
 	shard := 0
 	if len(key) > 0 {
@@ -40,9 +42,11 @@ type Clerk struct {
 	config   shardctrler.Config
 	make_end func(string) *labrpc.ClientEnd
 	// You will have to modify this struct.
+	me  int
+	seq int
+	mu  sync.Mutex
 }
 
-//
 // the tester calls MakeClerk.
 //
 // ctrlers[] is needed to call shardctrler.MakeClerk().
@@ -50,24 +54,35 @@ type Clerk struct {
 // make_end(servername) turns a server name from a
 // Config.Groups[gid][i] into a labrpc.ClientEnd on which you can
 // send RPCs.
-//
 func MakeClerk(ctrlers []*labrpc.ClientEnd, make_end func(string) *labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.sm = shardctrler.MakeClerk(ctrlers)
 	ck.make_end = make_end
 	// You'll have to add code here.
+	ck.me = int(nrand())
+	ck.seq = 0
+
 	return ck
 }
 
-//
+func (ck *Clerk) AllocateIndex() int {
+	ck.mu.Lock()
+	ck.seq++
+	ret := ck.seq
+	ck.mu.Unlock()
+	return ret
+}
+
 // fetch the current value for a key.
 // returns "" if the key does not exist.
 // keeps trying forever in the face of all other errors.
 // You will have to modify this function.
-//
 func (ck *Clerk) Get(key string) string {
-	args := GetArgs{}
-	args.Key = key
+	args := GetArgs{
+		Key:     key,
+		ClerkId: ck.me,
+		Seq:     ck.AllocateIndex(),
+	}
 
 	for {
 		shard := key2shard(key)
@@ -95,16 +110,16 @@ func (ck *Clerk) Get(key string) string {
 	return ""
 }
 
-//
 // shared by Put and Append.
 // You will have to modify this function.
-//
 func (ck *Clerk) PutAppend(key string, value string, op string) {
-	args := PutAppendArgs{}
-	args.Key = key
-	args.Value = value
-	args.Op = op
-
+	args := PutAppendArgs{
+		Key:     key,
+		Value:   value,
+		Op:      op,
+		ClerkId: ck.me,
+		Seq:     ck.AllocateIndex(),
+	}
 
 	for {
 		shard := key2shard(key)
--- src/shardkv/server.go
+++ src/shardkv/server.go
@@ -1,17 +1,34 @@
 package shardkv
 
+import (
+	"log"
+	"sync"
+	"sync/atomic"
+	"time"
 
-import "6.824/labrpc"
-import "6.824/raft"
-import "sync"
-import "6.824/labgob"
-
-
+	"6.824/labgob"
+	"6.824/labrpc"
+	"6.824/raft"
+	"6.824/shardctrler"
+)
 
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	ClerkId int
+	Seq     int
+	Cmd     string
+	Key     string
+	Value   string
+}
+
+type informCh struct {
+	Err     string
+	Cmd     string
+	Value   string
+	ClerkId int
+	Seq     int
 }
 
 type ShardKV struct {
@@ -25,30 +42,170 @@ type ShardKV struct {
 	maxraftstate int // snapshot if log grows this big
 
 	// Your definitions here.
+	mck         *shardctrler.Clerk
+	dead        int32
+	lastApplied int
+	table       map[string]string
+	Maxreq      map[int]int
+	Inform      map[int]chan informCh
 }
 
+func (kv *ShardKV) CheckShardsGid(key string) bool {
+	shard := key2shard(key)
+	config := kv.mck.Query(-1)
+	gid := config.Shards[shard]
+	return kv.gid == gid
+}
+
+func (kv *ShardKV) CheckInform(Index int, ClerkId int, Seq int) chan informCh {
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+	if _, ok := kv.Inform[Index]; !ok {
+		kv.Inform[Index] = make(chan informCh, 1)
+	}
+	if _, ok := kv.Maxreq[ClerkId]; !ok {
+		kv.Maxreq[ClerkId] = 0
+	}
+	return kv.Inform[Index]
+}
 
 func (kv *ShardKV) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	if !kv.CheckShardsGid(args.Key) {
+		reply.Err = ErrWrongGroup
+	}
+	index, _, isLeader := kv.rf.Start(Op{
+		ClerkId: args.ClerkId,
+		Seq:     args.Seq,
+		Cmd:     "Get",
+		Key:     args.Key,
+		Value:   "",
+	})
+	if isLeader {
+		ch := kv.CheckInform(index, args.ClerkId, args.Seq)
+		select {
+		case info := <-ch:
+			reply.Err = Err(info.Err)
+			kv.mu.Lock()
+			close(ch)
+			delete(kv.Inform, index)
+			kv.mu.Unlock()
+			reply.Value = info.Value
+		case <-time.After(50 * time.Millisecond):
+			reply.Err = "Timeout"
+		}
+	} else {
+		reply.Err = "ErrWrongLeader"
+	}
 }
 
 func (kv *ShardKV) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	if !kv.CheckShardsGid(args.Key) {
+		reply.Err = ErrWrongGroup
+	}
+	index, _, isLeader := kv.rf.Start(Op{
+		ClerkId: args.ClerkId,
+		Seq:     args.Seq,
+		Cmd:     args.Op,
+		Key:     args.Key,
+		Value:   args.Value,
+	})
+	if isLeader {
+		ch := kv.CheckInform(index, args.ClerkId, args.Seq)
+		select {
+		case info := <-ch:
+			reply.Err = Err(info.Err)
+			kv.mu.Lock()
+			close(ch)
+			delete(kv.Inform, index)
+			kv.mu.Unlock()
+		case <-time.After(50 * time.Millisecond):
+			reply.Err = "Timeout"
+		}
+	} else {
+		reply.Err = "ErrWrongLeader"
+	}
 }
 
-//
 // the tester calls Kill() when a ShardKV instance won't
 // be needed again. you are not required to do anything
 // in Kill(), but it might be convenient to (for example)
 // turn off debug output from this instance.
-//
 func (kv *ShardKV) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
 }
 
+func (kv *ShardKV) killed() bool {
+	z := atomic.LoadInt32(&kv.dead)
+	return z == 1
+}
 
-//
+func (kv *ShardKV) CheckClerkOpDuplicate(Clerk, Seq int) bool {
+	if _, ok := kv.Maxreq[Clerk]; !ok {
+		kv.Maxreq[Clerk] = 0
+	}
+	if kv.Maxreq[Clerk] >= Seq {
+		return true
+	} else {
+		kv.Maxreq[Clerk] = Seq
+		return false
+	}
+}
+
+func (kv *ShardKV) applier() {
+	for !kv.killed() {
+		cmd := <-kv.applyCh
+		if cmd.CommandValid {
+			kv.mu.Lock()
+			op := cmd.Command.(Op)
+			if cmd.CommandIndex <= kv.lastApplied {
+				continue
+			}
+			kv.lastApplied = cmd.CommandIndex
+			var info = informCh{
+				Cmd:     op.Cmd,
+				ClerkId: op.ClerkId,
+				Seq:     op.Seq,
+				Err:     "",
+			}
+			switch op.Cmd {
+			case "Get":
+				if v, ok := kv.table[op.Key]; ok {
+					info.Value = v
+				} else {
+					info.Value = ""
+				}
+			case "Put":
+				if kv.CheckClerkOpDuplicate(op.ClerkId, op.Seq) {
+					info.Err = "Duplicate"
+				} else {
+					kv.table[op.Key] = op.Value
+				}
+			case "Append":
+				if kv.CheckClerkOpDuplicate(op.ClerkId, op.Seq) {
+					info.Err = "Duplicate"
+				} else {
+					if v, ok := kv.table[op.Key]; ok {
+						kv.table[op.Key] = v + op.Value
+					} else {
+						kv.table[op.Key] = op.Value
+					}
+				}
+			default:
+				log.Fatal("Error cmd")
+			}
+			_, isLeader := kv.rf.GetState()
+			ch, ok := kv.Inform[cmd.CommandIndex]
+			if ok && isLeader {
+				ch <- info
+			}
+			kv.mu.Unlock()
+		}
+	}
+}
+
 // servers[] contains the ports of the servers in this group.
 //
 // me is the index of the current server in servers[].
@@ -75,7 +232,6 @@ func (kv *ShardKV) Kill() {
 //
 // StartServer() must return quickly, so it should start goroutines
 // for any long-running work.
-//
 func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister, maxraftstate int, gid int, ctrlers []*labrpc.ClientEnd, make_end func(string) *labrpc.ClientEnd) *ShardKV {
 	// call labgob.Register on structures you want
 	// Go's RPC library to marshall/unmarshall.
@@ -91,11 +247,17 @@ func StartServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persister,
 	// Your initialization code here.
 
 	// Use something like this to talk to the shardctrler:
-	// kv.mck = shardctrler.MakeClerk(kv.ctrlers)
+	kv.mck = shardctrler.MakeClerk(kv.ctrlers)
 
 	kv.applyCh = make(chan raft.ApplyMsg)
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
+	kv.table = make(map[string]string)
+	kv.Inform = make(map[int]chan informCh)
+	kv.Maxreq = make(map[int]int)
+	kv.lastApplied = 0
+
+	go kv.applier()
 
 	return kv
 }
